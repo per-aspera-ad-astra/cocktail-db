b5510108a8f957728703d6acba8a995bdb8710c1
alexey <alex.vasilenko13@gmail.com>
2020-05-23 23:07:03 +0300

    Updates

---
+++ precache-manifest.08b8a1f76950e1534a1b60f63c297672.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ce6b4cb23ee17eb34ea8340d22fc81",
+    "url": "/cocktail-db/index.html"
+  },
+  {
+    "revision": "ea9f91122cbb21f470af",
+    "url": "/cocktail-db/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "8630e5aa8ad2f053eb67",
+    "url": "/cocktail-db/static/css/main.09dd576e.chunk.css"
+  },
+  {
+    "revision": "ea9f91122cbb21f470af",
+    "url": "/cocktail-db/static/js/2.cb3ce8ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cocktail-db/static/js/2.cb3ce8ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8630e5aa8ad2f053eb67",
+    "url": "/cocktail-db/static/js/main.628107cd.chunk.js"
+  },
+  {
+    "revision": "d4868ee6ae0b55a2d9db",
+    "url": "/cocktail-db/static/js/runtime-main.f8afa513.js"
+  },
+  {
+    "revision": "23d0060a4de09f59bb5e0c544a03a673",
+    "url": "/cocktail-db/static/media/icon-checkbox-checked.23d0060a.svg"
+  },
+  {
+    "revision": "4538dcaf5a9fb1ee18369fd1b9173705",
+    "url": "/cocktail-db/static/media/icon-checkbox.4538dcaf.svg"
+  },
+  {
+    "revision": "680375595a12d0bb066ef3c46b6b44f4",
+    "url": "/cocktail-db/static/media/logo-cocktail.68037559.svg"
+  },
+  {
+    "revision": "448e0876b5337fb3693e595433ac7b08",
+    "url": "/cocktail-db/static/media/logo.448e0876.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/cocktail-db/precache-manifest.08b8a1f76950e1534a1b60f63c297672.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cocktail-db/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
